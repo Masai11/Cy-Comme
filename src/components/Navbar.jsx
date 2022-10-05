a6b68011663ee190c461d182e4b303829f8956af
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-10-05 14:55:36 +0300

    style the logo components and Navlinks section of the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 
-import { Search } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
 
@@ -12,44 +13,70 @@ const Wrapper = styled.div`
     padding: 10px;
     display:flex;
     justify-content: space-between;
+    align-items: center;
 `;
 
-const Logo = styled.div`
+const LogoSec = styled.div`
   flex: 1;
+`;
+const Logo = styled.h1`
+font-weight: bold;
 `;
 const SearchBar = styled.div`
   flex: 1;
   display:flex;
   align-items: center;
+  text-align: center;
   
 `;
 const Lang = styled.span`
-  font-size: 14px ;
+  font-size: 20px ;
   cursor: pointer;
 `;
 const SearchCont = styled.div`
-  border: 1px solid lightgray;
+  border: 0.5px solid lightgray;
   display:flex;
   align-items: center;
+  margin-left: 15px;
+  padding:5px;
+  t
 `;
+const Input = styled.input`
+  border : none;
+`;
+
 const NavLinks = styled.div`
   flex: 1;
+  display:flex;
+  justify-content:space-evenly;
   
+`;
+const Links = styled.div`
+font-size : 20px;
+cursor :pointer;
 `;
 
 const Navbar = () => {
   return (
     <Cont>
       <Wrapper>
-        <Logo>Cy-Comme</Logo>
+        <LogoSec><Logo>Cy-Comme.</Logo></LogoSec>
         <SearchBar>
           <Lang>EN</Lang>
           <SearchCont>
-              <Search/>
-              input
+              <Search style = {{color:"gray",fontSize:18 }}/>
+              <Input/>
           </SearchCont>
         </SearchBar>
-        <NavLinks>Register</NavLinks>
+        <NavLinks>
+          <Links>REGISTER</Links>
+          <Links>SIGN-IN</Links>
+          <Links>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined/>
+            </Badge>
+          </Links>
+        </NavLinks>
       </Wrapper>
     </Cont>
   )
