1a0cf7867b167f8cb241df9c959832c54738533e
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-10-05 21:53:06 +0300

    update description for category itemms

--- src/Data.js
+++ src/Data.js
@@ -3,22 +3,40 @@ export const sliderItems = [
     id: 1,
     img: "https://images.pexels.com/photos/1020052/pexels-photo-1020052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     Header: "SUMMER WEAR",
-    Description: "SHOP NOW!STYLE UP YOUR SUMMER WITH A 30% DISCOUNT FOR NEW ARRIVALS.",
+    Description: "SHOP NOW!!!  STYLE UP YOUR SUMMER WITH A 30% DISCOUNT FOR NEW ARRIVALS.",
     backgroundColor: "fcf1ed",
   },
   {
     id: 2,
     img: "https://images.pexels.com/photos/914668/pexels-photo-914668.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     Header: "POPULAR WEAR",
-    Description: "SHOP NOW!STYLE UP YOUR SEMI-CASUAL LOOK WITH A 30% DISCOUNT FOR NEW ARRIVALS.",
+    Description: "SHOP NOW!!!  STYLE UP YOUR SEMI-CASUAL LOOK WITH A 30% DISCOUNT FOR NEW ARRIVALS.",
     backgroundColor: "f5fafd",
   },
   {
     id: 3,
     img: "https://images.pexels.com/photos/7419238/pexels-photo-7419238.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     Header: "AUTUMN WEAR",
-    Description: "SHOP NOW!STYLE UP YOUR AUTUMN WITH A 30% DISCOUNT FOR NEW ARRIVALS.",
+    Description: "SHOP NOW!!!  STYLE UP YOUR AUTUMN WITH A 30% DISCOUNT FOR NEW ARRIVALS.",
     backgroundColor: "fbf0f4",
   },
   
 ]
+
+export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/10041258/pexels-photo-10041258.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      header: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/1375849/pexels-photo-1375849.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      header: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/2042455/pexels-photo-2042455.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      header: "LIGHT JACKETS",
+    },
+  ];
