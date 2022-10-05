1bca112900c00352600c48a20add25b9404f885c
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-10-05 17:22:38 +0300

    Create an array of items to pass my slider as props

---
+++ src/Data.js
@@ -0,0 +1,24 @@
+export const sliderItems = [
+    {
+    id: 1,
+    img: "https://images.pexels.com/photos/1020052/pexels-photo-1020052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    Header: "SUMMER WEAR",
+    Description: "SHOP NOW!STYLE UP YOUR SUMMER WITH A 30% DISCOUNT FOR NEW ARRIVALS.",
+    backgroundColor: "fcf1ed",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/914668/pexels-photo-914668.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    Header: "POPULAR WEAR",
+    Description: "SHOP NOW!STYLE UP YOUR SEMI-CASUAL LOOK WITH A 30% DISCOUNT FOR NEW ARRIVALS.",
+    backgroundColor: "f5fafd",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/7419238/pexels-photo-7419238.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    Header: "AUTUMN WEAR",
+    Description: "SHOP NOW!STYLE UP YOUR AUTUMN WITH A 30% DISCOUNT FOR NEW ARRIVALS.",
+    backgroundColor: "fbf0f4",
+  },
+  
+]
