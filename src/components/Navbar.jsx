aa56a03cb07e9ea722b75bb7399a10d1a9a307d3
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-10-05 13:09:14 +0300

    add navbar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const Cont = styled.div`
+    height: 60px;
+    
+`
+const Wrapper = styled.div`
+    padding: 10px;
+    display:flex;
+    justify-content: space-between;
+`;
+
+const Logo = styled.div`
+
+`;
+const Search = styled.div`
+  
+`;
+const NavLinks = styled.div`
+  
+`;
 
 const Navbar = () => {
   return (
-    <div>
-        
-    </div>
+    <Cont>
+      <Wrapper>
+        <Logo>Cy-Comme</Logo>
+        <Search>Search</Search>
+        <NavLinks>Register</NavLinks>
+      </Wrapper>
+    </Cont>
   )
 }
 
