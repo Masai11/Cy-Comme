5b42eade312112f931c7a6602da754ae58a7f2d9
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-10-06 12:00:09 +0300

    creaate fitler options for color and size and price

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
-import Produ
+import Products from '../components/Products'
 
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const Cont = styled.div``;
 
@@ -16,7 +17,19 @@ const FilterCont = styled.div`
 `;
 
 const Filter = styled.div`
-    margin:20px;`;
+    margin:20px;
+    `;
+const FilterTxt = styled.span`
+    font-size:20px;
+    font-weight:600;
+    margin-right:20px;
+`;
+
+const  Select = styled.select`
+    padding:10px;
+    margin-right:20px;
+`;
+const Option = styled.option``;
 
 const ProductList = () => {
   return (
@@ -24,11 +37,41 @@ const ProductList = () => {
         <Navbar/>
         <Header>Dresses</Header>
         <FilterCont>
-            <Filter>Filter1</Filter>
-            <Filter>Filter2</Filter>
+            <Filter><FilterTxt>Filter Products:</FilterTxt></Filter>
+            <Select>
+                <Option disabled selected>
+                    Color
+                </Option>
+                <Option>White</Option>
+                <Option>Black</Option>
+                <Option>Red</Option>
+                <Option>Blue</Option>
+                <Option>Yellow</Option>
+                <Option>Green</Option>
+            </Select>
+            <Select>
+                <Option disabled selected>
+                    Size
+                </Option>
+                <Option>XL</Option>
+                <Option>L</Option>
+                <Option>M</Option>
+                <Option>S</Option>
+                <Option>XS</Option>
+                
+            </Select>
+            <Filter><FilterTxt>Sort Products:</FilterTxt></Filter>
+            <Select>
+                <Option selected>New</Option>
+                <Option>Price(asc)</Option>
+                <Option>Price(desc)</Option>
+                
+                
+            </Select>  
         
         </FilterCont>
         <Products/>
+        <Footer/>
         
         
     </Cont>
